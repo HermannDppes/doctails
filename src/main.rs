78c839cc60b01c4d088a66d86424eb21402356e1
Hermann Döppes <hermannd.ouml.ppes@gmail.com>
2018-09-02 16:42:15 +0200

    Parsing unused arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 extern crate uuid;
 use uuid::Uuid;
 
+#[macro_use]
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
+	let app = App::new("doctails")
+		.about("Pins Tails on Docs.")
+		.author("Hermann Döppes <hermannd.ouml.ppes@gmail.com>")
+		.version(crate_version!());
+	let adopt = SubCommand::with_name("adopt")
+		.about("Adopt a new Doc.")
+		.arg(Arg::with_name("PATH")
+			.help("Path to the document / blob that should be taken in")
+			.required(true)
+		);
+	let matches = app
+		.subcommand(adopt)
+		.get_matches();
 	let uuid = Uuid::new_v4();
 	println!("{}", uuid.hyphenated());
 }
