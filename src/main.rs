8edd90f7a5647c590f7faeb32b07cdaddc693a7f
Hermann Döppes <hermannd.ouml.ppes@gmail.com>
2018-09-02 21:48:38 +0200

    Echoing the PATH argument

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 extern crate uuid;
 use uuid::Uuid;
 
+use std::path::Path;
+
 #[macro_use]
 extern crate clap;
 use clap::{Arg, App, SubCommand};
+
+fn adopt(path: &Path) {
+	let uuid = Uuid::new_v4();
+	println!("{} adopted as {}", path.to_str().unwrap(), uuid.hyphenated());
+}
 
 fn main() {
 	let app = App::new("doctails")
 		.about("Pins Tails on Docs.")
 		.author("Hermann Döppes <hermannd.ouml.ppes@gmail.com>")
 		.version(crate_version!());
-	let adopt = SubCommand::with_name("adopt")
+	let sc_adopt = SubCommand::with_name("adopt")
 		.about("Adopt a new Doc.")
 		.arg(Arg::with_name("PATH")
 			.help("Path to the document / blob that should be taken in")
 			.required(true)
 		);
 	let matches = app
-		.subcommand(adopt)
+		.subcommand(sc_adopt)
 		.get_matches();
-	let uuid = Uuid::new_v4();
-	println!("{}", uuid.hyphenated());
+
+	if let Some(sub_matches) = matches.subcommand_matches("adopt") {
+		adopt(Path::new(sub_matches.value_of("PATH").unwrap()));
+	}
 }
