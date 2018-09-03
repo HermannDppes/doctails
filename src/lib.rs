ea3c78701b5124dcd4da4f35bf64f0d896063370
Hermann Döppes <hermannd.ouml.ppes@gmail.com>
2018-09-03 19:25:09 +0200

    Make Doc contain String instead of &'a OsStr
    This is a preparation for (De)Serialization of Pack.

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,32 @@ extern crate uuid;
 use uuid::Uuid;
 
 use std::path::Path;
-
 use std::ffi::OsStr;
 
 use std::collections::HashMap;
 
 #[derive(Debug)]
-struct Doc<'a> {
-	filename: &'a OsStr
+struct Doc {
+	filename: String
 }
 
 #[derive(Debug)]
-pub struct Pack<'a> {
-	docs: HashMap<Uuid, Doc<'a>>
+pub struct Pack {
+	docs: HashMap<Uuid, Doc>
 }
 
-impl<'a> Pack<'a> {
-	pub fn adopt(&'a mut self, path: &'a Path) {
+impl Pack {
+	pub fn adopt(&mut self, path: &Path) {
 		let uuid = Uuid::new_v4();
-		let doc = Doc {
-			filename: path.file_name().unwrap()
-		};
+		let filename = path.file_name().unwrap()
+			.to_string_lossy()
+			.into_owned();
+		let doc = Doc {filename};
 		self.docs.insert(uuid, doc);
 		println!("We are now {:?}", self);
 	}
 
-	pub fn new() -> Pack<'a> {
+	pub fn new() -> Pack {
 		Pack {
 			docs: HashMap::new()
 		}
